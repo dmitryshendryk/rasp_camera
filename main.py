63cba3cc5aa1ed549e3a3df3dbee272fae04af2f
Dmitry <shendryk.d@pg.com>
2019-12-27 18:48:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,96 @@
+from __future__ import division
+from subprocess import PIPE, Popen
+import psutil
+import struct
+import os
+os.environ['GPIOZERO_PIN_FACTORY'] = os.environ.get('GPIOZERO_PIN_FACTORY', 'mock')
+
+import time
+import socket
+import json
+from gpiozero import CPUTemperature
+
+
+
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+
+
+pid = os.getpid()
+print('MQTT client starting with PID {}..'.format(pid))
+
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
+
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+def get_rpi_monitoring_data():
+
+    cpu_usage = psutil.cpu_percent()
+
+    ram = psutil.virtual_memory()
+    ram_total = ram.total / 2**20       # MiB.
+    ram_used = ram.used / 2**20
+    ram_free = ram.free / 2**20
+    ram_percent_used = ram.percent
+
+    disk = psutil.disk_usage('/')
+    disk_total = disk.total / 2**30     # GiB.
+    disk_used = disk.used / 2**30
+    disk_free = disk.free / 2**30
+    disk_percent_used = disk.percent
+    cpu = CPUTemperature()
+
+    monitor = {'cpu_usage': cpu_usage, 'ram_percent_used': ram_percent_used, 'disk_percent_used': disk_percent_used,
+			'cpu_temperature': cpu.temperature}
+
+    return monitor
+
+
+
+
+mqttc = mqtt.Client()
+mqttc.username_pw_set("munich_broker", "8Delusion-Serif")
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+mqttc.connect("18.185.47.167", 1883, 60)
+#mqttc.subscribe("rpi/status", 0)
+
+mqttc.loop_start()
+
+
+seq = 1
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+while True:
+    # payload = struct.pack(FORMAT, pid, seq, os.urandom(8))
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    blob = get_rpi_monitoring_data()
+    blob['connectionStatus'] = True
+    blob['id'] = 2
+    blob = json.dumps(blob)
+    result, mid = mqttc.publish('rpi', blob, qos=0, retain=True)
+    print('event published: result={}, mid={}'.format(result, mid))
+
+    seq += 1
+    time.sleep(1)

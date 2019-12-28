f43f16587241998bad104c0258456231d3057c1d
Dmitry <shendryk.d@pg.com>
2019-12-28 14:09:16 +0300

    split py

--- mqtt_client.py
+++ mqtt_client.py
@@ -9,41 +9,41 @@ class MQTTClient():
     def __init__(self):
         self.mqttc = mqtt.Client()
         self.mqttc.username_pw_set(Config.MQTT_USER, Config.MQTT_PASS)
-        self.mqttc.on_message = on_message
-        self.mqttc.on_connect = on_connect
-        self.mqttc.on_publish = on_publish
-        self.mqttc.on_subscribe = on_subscribe
+        self.mqttc.on_message = self.on_message
+        self.mqttc.on_connect = self.on_connect
+        self.mqttc.on_publish = self.on_publish
+        self.mqttc.on_subscribe = self.on_subscribe
         self.mqttc.connect(Config.MQTT_HOST, Config.MQTT_PORT, Config.MQTT_KEEP_ALIVE)
         self.mqttc.subscribe("store/prishna/rpi/actions/reboot", qos=1)
 
-        self.mqttc.message_callback_add("store/prishna/rpi/actions/reboot", reboot_rpi)
+        self.mqttc.message_callback_add("store/prishna/rpi/actions/reboot", self.reboot_rpi)
     
 
-    def on_connect(mqttc, obj, flags, rc):
+    def on_connect(self, mqttc, obj, flags, rc):
         print("rc: " + str(rc))
 
 
-    def on_message(mqttc, obj, msg):
+    def on_message(self, mqttc, obj, msg):
         print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-    def on_command(mqttc, obj, msg):
+    def on_command(self, mqttc, obj, msg):
         print('Command topics')
         print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 
-    def on_publish(mqttc, obj, mid):
+    def on_publish(self, mqttc, obj, mid):
         print("mid: " + str(mid))
 
 
-    def on_subscribe(mqttc, obj, mid, granted_qos):
+    def on_subscribe(self, mqttc, obj, mid, granted_qos):
         print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 
-    def on_log(mqttc, obj, level, string):
+    def on_log(self, mqttc, obj, level, string):
         print(string)
 
 
-    def reboot_rpi(mqttc, obj, msg):
+    def reboot_rpi(self, mqttc, obj, msg):
         msg.payload = int(msg.payload)
         rpi_id = int(os.environ['RPI_ID'])
         if rpi_id == msg.payload:

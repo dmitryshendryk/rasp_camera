9b55772c427537f88dec5f3c1b144a5cbbbdda4e
Dmitry <shendryk.d@pg.com>
2019-12-29 19:48:25 +0300

    set event

--- camera.py
+++ camera.py
@@ -18,34 +18,40 @@ class VideoGet:
 
 
     def start(self):
-        if not self.stream:
-            self.stream = cv2.VideoCapture(0)
+        print('Start thread')
+        if self.stopped.is_set():
+            self.stopped.clear()
         now = datetime.now()
         now_date = now.strftime("%d_%m_%Y__%H_%M_%S")
         fourcc = cv2.VideoWriter_fourcc(*"H264")
         file_name = str(now_date) + '.avi'
         self.out = cv2.VideoWriter(file_name, fourcc, 20.0, (640,480))
+        print('Start T')
         t = Thread(target=self.get, args=(self.stopped, ))
         t.setDaemon(True)
         t.start()
+        print('Started T')
         return self
 
     def get(self, stopped):
+        print('Inside get')
         while not stopped.is_set():
             if not self.grabbed:
                 self.stop()
             else:
                 (self.grabbed, self.frame) = self.stream.read()
                 if self.grabbed:
+                    print('Writting frames')
                     self.out.write(self.frame)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
     def stop(self):
-        self.stream.release()
+        print('Stoping thread')
+        # self.stream.release()
         self.stopped.set()
         # self.stopped = True
-
+        print('Finished thread')
     def __del__(self):
         self.stream.release()
         cv2.destroyAllWindows()

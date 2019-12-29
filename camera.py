d65509325df94519fdfbbf9b097182949f5ed689
Dmitry <shendryk.d@pg.com>
2019-12-29 19:32:31 +0300

    set event

--- camera.py
+++ camera.py
@@ -22,7 +22,7 @@ class VideoGet:
         now_date = now.strftime("%d_%m_%Y__%H_%M_%S")
         fourcc = cv2.VideoWriter_fourcc(*"H264")
         file_name = str(now_date) + '.avi'
-        out = cv2.VideoWriter(file_name, fourcc, 20.0, (640,480))
+        self.out = cv2.VideoWriter(file_name, fourcc, 20.0, (640,480))
         t = Thread(target=self.get, args=(self.stopped, ))
         t.setDaemon(True)
         t.start()

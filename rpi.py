453c6db3c2bfdeedf51b78fb18f15283b9b62695
Dmitry <shendryk.d@pg.com>
2020-02-03 22:28:48 +0300

    add band

--- rpi.py
+++ rpi.py
@@ -7,38 +7,37 @@ import time
 
 
 
-count=0
-qry=''
 
-ul=0.00
-dl=0.00
-t0 = time.time()
-upload=psutil.net_io_counters(pernic=True)['wlan0'][0]
-download=psutil.net_io_counters(pernic=True)['wlan0'][1]
-up_down=(upload,download)
+
 
 
 class RPI:
 
     def __init__(self):
         pass
-
+        self.count = 0 
+        self.upload=psutil.net_io_counters(pernic=True)['wlan0'][0]
+        self.download=psutil.net_io_counters(pernic=True)['wlan0'][1]
+        self.up_down=(upload,download)
+        self.ul=0.00
+        self.dl=0.00
+        self.t0 = time.time()
 
     def get_rpi_monitoring_data(self):
 
 
-        last_up_down = up_down
-        upload=psutil.net_io_counters(pernic=True)['wlan0'][0]
-        download=psutil.net_io_counters(pernic=True)['wlan0'][1]
+        last_up_down = self.up_down
+        self.upload=psutil.net_io_counters(pernic=True)['wlan0'][0]
+        self.download=psutil.net_io_counters(pernic=True)['wlan0'][1]
         t1 = time.time()
-        up_down = (upload,download)
+        self.up_down = (self.upload,self.download)
         try:
-            ul, dl = [(now - last) / (t1 - t0) / 1024.0
-                    for now,last in zip(up_down, last_up_down)]             
+            self.ul, self.dl = [(now - last) / (t1 - self.t0) / 1024.0
+                    for now,last in zip(self.up_down, last_up_down)]             
             t0 = time.time()
         except:
             pass
-        if dl>0.1 or ul>=0.1:
+        if self.dl>0.1 or self.ul>=0.1:
             time.sleep(0.25) 
         # print('UL: {:0.2f} kB/s \n'.format(ul)+'DL: {:0.2f} kB/s'.format(dl))
 
@@ -60,6 +59,6 @@ class RPI:
         now = datetime.now()
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         monitor = {'time': date_time, 'cpu_usage': cpu_usage, 'ram_percent_used': ram_percent_used, 'disk_percent_used': disk_percent_used,
-                'cpu_temperature': cpu.temperature, 'ul': ul, 'dl': dl}
+                'cpu_temperature': cpu.temperature, 'ul': self.ul, 'dl': self.dl}
 
         return monitor

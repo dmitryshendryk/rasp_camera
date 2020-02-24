afbe6a9f87042c72a0c809daa94b8c63d22e51ea
Dmitry <shendryk.d@pg.com>
2020-02-24 20:23:36 +0300

    add location

--- mqtt_client.py
+++ mqtt_client.py
@@ -239,7 +239,7 @@ class MQTTClient():
             
             now = datetime.now()
             date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-            blob = json.dumps({'time': str(now), 'node': self.rpi_id, 'node_type': self.local_config['type'], 'log': 'Clear Videos'})
+            blob = json.dumps({'time': str(now), 'region':self.local_config['location'], 'node': self.rpi_id, 'node_type': self.local_config['type'], 'log': 'Clear Videos'})
             self.publish_message('/logs/rpi/' + self.local_config['type'] + '/', blob)
 
 
